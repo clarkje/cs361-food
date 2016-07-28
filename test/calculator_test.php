82b4bb57f9dde3671b61a5c610aeb661e131920d
Jeromie Clark <clarkje@oregonstate.edu>
2016-07-27 17:18:06 -0700

    PHPUnit Example
    Here's a quick example of a Calculator class and a corresponding
    CalculatorTest Unit Test class.
    
    You can run it with:
    ../vendor/eher/phpunit/bin/phpunit calculator_test.php

---
+++ test/calculator_test.php
@@ -0,0 +1,14 @@
+<?php 
+
+require 'calculator.php';
+
+class CalculatorTest extends PHPUnit_Framework_TestCase { 
+
+	public function testAdd() { 
+
+		$c = New Calculator;
+		$result = $c->add(5, 10);
+		$this->assertEquals(15, $result); 
+	}
+}
+?>	
