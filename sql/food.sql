84301a660b9254c305d43dec1f6acabfb7b5c34f
Jeromie Clark <clarkje@oregonstate.edu>
2016-07-28 18:38:49 +0000

    SQL for database schema

---
+++ sql/food.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--
+-- Host: 0.0.0.0    Database: food
+-- ------------------------------------------------------
+-- Server version	5.5.49-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gmo`
+--
+
+DROP TABLE IF EXISTS `gmo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gmo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `m_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `sci_name` varchar(255) NOT NULL,
+  `description` text,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `m_id` (`m_id`),
+  CONSTRAINT `gmo_ibfk_1` FOREIGN KEY (`m_id`) REFERENCES `manufacturer` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gmo`
+--
+
+LOCK TABLES `gmo` WRITE;
+/*!40000 ALTER TABLE `gmo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gmo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturer`
+--
+
+DROP TABLE IF EXISTS `manufacturer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manufacturer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `website_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturer`
+--
+
+LOCK TABLES `manufacturer` WRITE;
+/*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
+INSERT INTO `manufacturer` VALUES (20,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(21,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(23,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(25,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(27,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(29,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/'),(31,'Foo','123-456-7890','foo@bar.com','http://www.foo.com/');
+/*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-28 18:38:06
